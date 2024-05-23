d7ff51e450f7d8908f01650ad46801e1d7107d8f
POO <agusmaty23@gmail.com>
2024-05-23 07:57:34 -0500

    Repositorio

---
+++ MAIN.java
@@ -0,0 +1,8 @@
+package Clase6;
+
+public class MAIN {
+    public static void main(String[] args) {
+        System.out.println("GITHUB REPOSITORY");
+        System.out.println("TEST OUTEST");
+    }
+}
