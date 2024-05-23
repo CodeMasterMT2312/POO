e893e3c1741df97bcc7066bbbe48e98db474953d
POO <agusmaty23@gmail.com>
2024-05-23 09:23:23 -0500

    Corregir datos a imprimir

--- CLASE_HEREDADA/Main.java
+++ CLASE_HEREDADA/Main.java
@@ -2,7 +2,7 @@ package GIT_POO.CLASE_HEREDADA;
 
 public class Main {
     public static void main(String[] args){
-        Carro carro1 = new Carro("10 metro","Carro","Overlanding","Toyota","Tacoma","Verde","ABC-123","Jose Luis");
+        Carro carro1 = new Carro("10 metros","Carro","Overlanding","Toyota","Tacoma","Verde","ABC-123","Jose Luis");
         carro1.imprimirC();
     }
 }
