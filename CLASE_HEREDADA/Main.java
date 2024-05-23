bdebdb533a8374ace6a5e2508003cffa0735f300
POO <agusmaty23@gmail.com>
2024-05-23 09:40:48 -0500

    Agregar  HIJA 1 HIJA

--- CLASE_HEREDADA/Main.java
+++ CLASE_HEREDADA/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args){
         Carro carro1 = new Carro("10 metros","Carro","Overlanding","Toyota","Tacoma","Verde","ABC-123","Jose Luis");
         carro1.imprimirC();
+        System.out.println("\n");
+        Carro_Revision carro1Rev = new Carro_Revision("10 metros","Carro","Overlanding","Toyota","Tacoma","Verde","ABC-123","Jose Luis",false,"No paso por gases");
+        carro1Rev.ImprimirRev();
     }
 }
--- CLASE_HEREDADA/Vehiculos.java
+++ CLASE_HEREDADA/Vehiculos.java
@@ -41,3 +41,23 @@ class Carro extends Vehiculos{
         System.out.println("Marca: "+Marca+" Modelo: "+Modelo+" Color: "+Color+" Placa: "+Placa+" Propietario: "+Propietario);
     }
 }
+
+class Carro_Revision extends Carro{
+    //Atributos
+    Boolean Paso_Revision;
+    String Fallas;
+    //Constructor
+
+    public Carro_Revision(String tamaño, String tipo, String uso, String marca, String modelo, String color, String placa, String propietario, Boolean paso_Revision, String fallas) {
+        super(tamaño, tipo, uso, marca, modelo, color, placa, propietario);
+        this.Paso_Revision = paso_Revision;
+        this.Fallas = fallas;
+    }
+
+    //Metodos
+
+    public void ImprimirRev(){
+        super.imprimirC();
+        System.out.println("Paso Revision: "+Paso_Revision+" Fallas: "+Fallas);
+    }
+}
