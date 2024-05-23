9628cff50247414dfbba6f4155324485cdceb7c1
POO <agusmaty23@gmail.com>
2024-05-23 09:19:45 -0500

    Agregar archivos Herencia

---
+++ CLASE_HEREDADA/Main.java
@@ -0,0 +1,8 @@
+package GIT_POO.CLASE_HEREDADA;
+
+public class Main {
+    public static void main(String[] args){
+        Carro carro1 = new Carro("10 metro","Carro","Overlanding","Toyota","Tacoma","Verde","ABC-123","Jose Luis");
+        carro1.imprimirC();
+    }
+}
---
+++ CLASE_HEREDADA/Vehiculos.java
@@ -0,0 +1,43 @@
+package GIT_POO.CLASE_HEREDADA;
+
+public class Vehiculos {
+    //Atributos
+    String tipo;
+    String Tamaño;
+    String Uso;
+
+    public Vehiculos(String tamaño, String tipo, String uso) {
+        this.Tamaño = tamaño;
+        this.tipo = tipo;
+        this.Uso = uso;
+    }
+
+    //Metodos
+    public void imprimir(){
+        System.out.println("Tamaño: "+Tamaño+" Tipo: "+tipo);
+    }
+}
+class Carro extends Vehiculos{
+    //Atributos
+    String Marca;
+    String Modelo;
+    String Color;
+    String Placa;
+    String Propietario;
+
+    public Carro(String tamaño, String tipo, String uso, String marca, String modelo,String color, String placa,String propietario){
+        super(tamaño, tipo, uso);
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Color = color;
+        this.Placa = placa;
+        this.Propietario = propietario;
+
+    }
+
+    //Metodos
+    public void imprimirC(){
+        super.imprimir();
+        System.out.println("Marca: "+Marca+" Modelo: "+Modelo+" Color: "+Color+" Placa: "+Placa+" Propietario: "+Propietario);
+    }
+}
