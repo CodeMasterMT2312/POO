2a614de7153a307ae81a5a945c5022a426229cd4
POO <agusmaty23@gmail.com>
2024-05-23 08:35:36 -0500

    Borrar codigo

--- MAIN.java
+++ MAIN.java
@@ -3,6 +3,6 @@ package Clase6;
 public class MAIN {
     public static void main(String[] args) {
         System.out.println("GITHUB REPOSITORY");
-        System.out.println("TEST OUTEST");
+
     }
 }
