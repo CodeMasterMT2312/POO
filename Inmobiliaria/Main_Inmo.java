fca402390df3e37a640f08f8a0b921fcc2768452
POO <agusmaty23@gmail.com>
2024-05-31 12:04:18 -0500

    Creacion del main

---
+++ Inmobiliaria/Main_Inmo.java
@@ -0,0 +1,8 @@
+package GIT_POO.Inmobiliaria;
+
+public class Main_Inmo {
+    public static void main(String[] args) {
+        Departamento depa1 = new Departamento("Madrid",45000.90,3,65.8,7);
+        depa1.MostrarInformacion();
+    }
+}
