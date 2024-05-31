2f25862a9e120d71b2d34c854de3d85ca347a5e1
POO <agusmaty23@gmail.com>
2024-05-31 11:46:07 -0500

    Agregar Ejercicios

--- Ejercicios_Calculos/Calculos.java
+++ Ejercicios_Calculos/Calculos.java
@@ -1,2 +1,51 @@
-package GIT_POO.Ejercicios_Calculos;public class Calculos {
+package GIT_POO.Ejercicios_Calculos;
+
+public class Calculos {
+    //Atributos
+    private int Numero1;
+    private int Numero2;
+    private int Numero3;
+
+    //Constructor
+    public Calculos(int numero1, int numero2, int numero3) {
+        this.Numero1 = numero1;
+        this.Numero2 = numero2;
+        this.Numero3 = numero3;
+    }
+    //Metodos
+
+
+    public int getNumero1() {
+        return Numero1;
+    }
+
+    public int getNumero3() {
+        return Numero3;
+    }
+
+    public int getNumero2() {
+        return Numero2;
+    }
+
+    private void Mostrar(){
+        System.out.println("Numero 1: "+Numero1);
+        System.out.println("Numero 2: "+Numero2);
+        System.out.println("Numero 3: "+Numero3);
+    }
+    public void LlamarFuncion(){
+        Mostrar();
+    }
+}
+
+class Sumar extends Calculos{
+    public Sumar(int numero1, int numero2, int numero3) {
+        super(numero1, numero2, numero3);
+    }
+    //Metodos
+    public int SumarNum() {
+        super.LlamarFuncion();
+        int suma = getNumero1() + getNumero2() + getNumero3();
+        System.out.println("Suma :" + suma);
+        return suma;
+    }
 }
--- Ejercicios_Calculos/Main_Cal.java
+++ Ejercicios_Calculos/Main_Cal.java
@@ -1,2 +1,8 @@
-package GIT_POO.Ejercicios_Calculos;public class Main_Cal {
+package GIT_POO.Ejercicios_Calculos;
+
+public class Main_Cal {
+    public static void main(String[] args) {
+        Sumar sum = new Sumar(15,20,15);
+        sum.SumarNum();
+    }
 }
